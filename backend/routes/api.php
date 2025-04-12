a576dbab145ff0cb04b0c1db73ceff5b8867958c
Volodymyr Bachynskyi <bachinsky1@gmail.com>
2025-04-12 12:41:38 +0300

    Auth logics addded

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +19,13 @@ use App\Http\Controllers\Api\OrderController;
 |
 */
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/orders', [OrderController::class, 'store']);
     Route::get('/orders', [OrderController::class, 'index']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::get('/products', [ProductController::class, 'index']);
